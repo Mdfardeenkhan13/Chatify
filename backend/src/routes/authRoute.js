0a52f538f4374db05082dd6929ad4a4439ef44a9
Mdfardeenkhan3 <mdfardeenkhan768@gmail.com>
2026-02-16 22:26:07 +0530

    Rate limit with Arcjet

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { loginController, logoutController, signupController, updateProfile } from "../controllers/authController.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
+// for rate limit 5 if we refrest page more tham 5 time then it dont show this message, instead it show error message: {"message":"Rate limit exceeded. Please try again later."}
+// router.get("/test", arcjetProtection, (req, res) => {
+//     res.status(200).json({ message: "Test route" });
+// })
+
+router.use(arcjetProtection);
+
 router.post("/signup", signupController);
-
-router.post("/login", loginController);
-
+router.post("/login", arcjetProtection, loginController);
 router.post("/logout", logoutController);
 
 router.put("/update-profile", protectRoute, updateProfile);
